a96924dc31079e052ec5efebfb25ed15259fba33
QiskitFallFest23-UNIPA <>
2023-10-08 19:33:06 +0000

    Update from https://github.com/QiskitFallFest23-UNIPA/QiskitFallFest23-UNIPA.github.io_src/commit/308899dec30e1ad5f7be7cecff0083e5f2b0ca74

---
+++ assets/js/cbpAnimatedHeader.js
@@ -0,0 +1,57 @@
+/**
+ * cbpAnimatedHeader.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2013, Codrops
+ * http://www.codrops.com
+ */
+var cbpAnimatedHeader = (function () {
+
+	var docElem = document.documentElement,
+		header = document.querySelector('#navbarHome.navbar-fixed-top'),
+		didScroll = false,
+		changeHeaderOn = 100;
+
+	var dropdown = document.querySelector('.dropdown-menu.dropdown-menu-custom');
+
+	function init() {
+		window.addEventListener('scroll', function (event) {
+			if (!didScroll) {
+				didScroll = true;
+				setTimeout(scrollPage, 250);
+			}
+		}, false);
+	}
+
+	function scrollPage() {
+		var sy = scrollY();
+		if (sy >= changeHeaderOn) {
+			if (header !== null) {
+				classie.add(header, 'navbar-shrink');
+			}
+			if (dropdown !== null) {
+				classie.add(dropdown, 'dropdown-menu-custom-shrink');
+			}
+		}
+		else {
+
+			if (header !== null && header.classList.contains('navbar-shrink')) {
+				classie.remove(header, 'navbar-shrink');
+			}
+			if (dropdown !== null && dropdown.classList.contains('dropdown-menu-custom-shrink')) {
+				classie.remove(dropdown, 'dropdown-menu-custom-shrink');
+			}
+		}
+		didScroll = false;
+	}
+
+	function scrollY() {
+		return window.pageYOffset || docElem.scrollTop;
+	}
+
+	init();
+
+})();
