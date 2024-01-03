15b35cd8e66ad14b5e673f59803c5487e9b86165
QiskitFallFest23-UNIPA <>
2024-01-03 18:47:32 +0000

    Update from https://github.com/QiskitFallFest23-UNIPA/QiskitFallFest23-UNIPA.github.io_src/commit/034812b6e1320dd5bc94a178ef338543fcf018fa

---
+++ assets/js/verification/verification.js
@@ -0,0 +1,221 @@
+// Fetch the public key from a static file
+async function fetchPublicKey(keyPath='/assets/keys/public_key.pem') {
+    try {
+        const response = await fetch(keyPath);  // Replace with the actual path to your public key file
+        const publicKey = await response.text();
+        return publicKey;
+    } catch (error) {
+        console.error('Error fetching public key:', error);
+        return null;
+    }
+
+}
+
+// Function to extract query parameters from the URL
+function getQueryParams(url) {
+    const params = {};
+    const urlSearchParams = new URLSearchParams(url.slice(url.indexOf('?') + 1));
+    for (const [key, value] of urlSearchParams) {
+        params[key] = value;
+    }
+    return params;
+}
+
+// Extract certificate ID and signature from the URL
+function getCertificateInfo(window) {
+    // Get the query string from the URL
+    const queryString = window.location.search;
+
+    if (queryString === '') {
+        console.warn('Query string not found in the URL.');
+        return null;
+    }
+
+    // Parse the query string into an object
+    const urlParams = new URLSearchParams(queryString);
+
+    const response = {};
+    for (const [key, value] of urlParams) {
+
+        if (key === 'id' || key === 'signature') {
+
+            // replace - with + and _ with / in the URL
+            // (the string was generated using base64urlsafe encoding)
+            const encodedString = value.replace(/-/g, '+').replace(/_/g, '/');
+
+            // from base64 string to string
+            try {
+                response[key] = atob(encodedString);
+            } catch (error) {
+                console.warn('Error decoding base64 string:', error);
+                response[key] = null;
+            }
+
+        } else {
+            response[key] = value;
+        }
+
+    }
+
+    if (!response.id) {
+        console.warn('Certificate ID not found in the URL.');
+    } else {
+        console.debug('Certificate ID:', response.id);
+    }
+
+    if (!response.signature) {
+        console.warn('Signature not available.');
+    } else {
+        console.debug('Signature found in the URL.');
+    }
+
+    return response;
+}
+
+/*
+Convert a string into an ArrayBuffer
+from https://developers.google.com/web/updates/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
+*/
+function str2ab(str) {
+    const buf = new ArrayBuffer(str.length);
+    const bufView = new Uint8Array(buf);
+    for (let i = 0, strLen = str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
+
+
+function importRsaKey(pem) {
+    // fetch the part of the PEM string between header and footer
+    const pemHeader = "-----BEGIN PUBLIC KEY-----";
+    const pemFooter = "-----END PUBLIC KEY-----";
+    const pemContents = pem.substring(
+        pemHeader.length,
+        pem.length - pemFooter.length - 1,
+    );
+    // base64 decode the string to get the binary data
+    const binaryDerString = window.atob(pemContents);
+    // convert from a binary string to an ArrayBuffer
+    const binaryDer = str2ab(binaryDerString);
+
+    // is window.crypto.subtle not defined?
+    if (window?.crypto?.subtle === undefined) {
+        console.error("WebCrypto API not available in this browser! Are you using HTTPS?");
+        throw new Error("WebCrypto API not available in this browser! Are you using HTTPS?");
+    }
+
+    return window.crypto.subtle.importKey(
+        "spki",
+        binaryDer,
+        {
+            name: "RSASSA-PKCS1-v1_5",
+            hash: "SHA-256",
+        },
+        true,
+        ["verify"],
+    );
+}
+
+// Function to verify the signature
+async function verifySignature_1(data, signature, publicKey) {
+
+
+    // Create a CryptoKey object from the public key
+    const cryptoKey = await importRsaKey(publicKey);
+
+    // Verify the signature using the retrieved certificate data and publicKey
+    // If the signature is valid, return true; otherwise, return false
+    const signatureBytes = Uint8Array.from(signature, c => c.charCodeAt(0));
+
+    const isValid = await crypto.subtle.verify(
+        { name: 'RSASSA-PKCS1-v1_5' },
+        cryptoKey,
+        signatureBytes,
+        new TextEncoder().encode(data)
+    );
+
+    return isValid;
+}
+
+
+// Function to verify the signature using the public key
+async function verifySignature_2(data, signature, publicKey) {
+
+    // Create a CryptoKey object from the public key
+    const cryptoKey = await importPrivateKey(publicKey);
+
+    // Compute the SHA-256 hash of the data
+    const encoder = new TextEncoder();
+    const dataBuffer = encoder.encode(data);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', dataBuffer);
+
+    // Verify the signature
+    const signatureBuffer = new Uint8Array(signature.match(/[\s\S]/g).map((ch) => ch.charCodeAt(0)));
+
+    const isValid = await crypto.subtle.verify(
+        { name: 'RSA-PSS', saltLength: 20 },
+        cryptoKey,
+        signatureBuffer,
+        hashBuffer
+    );
+
+    return isValid;
+}
+
+
+// Verify the certificate using the public key and signature
+async function verifyCertificate(certInfo) {
+    const publicKey = await fetchPublicKey();
+    const certId = certInfo.id;
+    const signature = certInfo.signature;
+    let exception = null;
+
+    if (!certId) {
+        console.warn("Certificate not verifiable!")
+        return {
+            isValid: null,
+            error: "Certificate not available."
+        }
+    }
+
+    if (!signature) {
+        console.warn("Certificate not verifiable!")
+        return {
+            isValid: null,
+            error: "Signature not available."
+        };
+    };
+
+    if (!publicKey) {
+        console.warn("Certificate not verifiable!");
+        return {
+            isValid: null,
+            error: "Public Key not available."
+        };
+    }
+
+    // Verify the signature
+    const isValid = await verifySignature_1(certId, signature, publicKey).then((result) => {
+        console.log('Certificate verification Result:', result);
+
+        if (result === true) {
+            // Display success message
+            console.log("Certificate is valid!");
+        } else {
+            // Display error message
+            console.warn("Certificate is not valid!");
+        }
+        return result;
+    }).catch((error) => {
+
+        console.warn(`Error verifying certificate: ${error}`);
+        exception = error;
+        return null;
+    });
+
+    return {
+        isValid: isValid,
+        error: exception
+    };
+}
---
+++ assets/js/verification/verification_page.js
@@ -0,0 +1,144 @@
+// Perform verification when the page loads
+//document.addEventListener("DOMContentLoaded", function () {    });
+
+window.onload = async function () {
+
+    // Get the certificate info from the URL
+    const certInfo = getCertificateInfo(window);
+
+    let resultMessage = ""
+    let isValid = null;
+
+    if (certInfo === null) {
+        console.warn('Certificate Info not found in the URL.');
+
+        // Display the verification result
+        resultMessage = 'Do you want to verify a certificate?<br>From the .pdf certificate you received, click on the link <code>Verify here</code> or scan the QR code.';
+    } else {
+        const certInfoObj = JSON.parse(certInfo.id); //from jsonstring to object
+        //const certInfoStr = JSON.stringify(certInfoObj); //from object to string
+
+        // Perform verification when the page loads
+        const response = await verifyCertificate(certInfo);
+        isValid = response.isValid;
+        console.debug("response:", response);
+
+        // Display the verification result
+        resultMessage = isValid ? `This certificate is valid and it was issued to <b>${certInfoObj.issued_to}</b> on <b>${certInfoObj.issued_on}</b>.` : isValid === false ? 'Sorry, but this certificate is not valid and we believe it was falsified!' : `Sorry, but an unexpected error occurred while verifying the certificate! Please contact the issuer of this certificate for more information.<br>Details: <code>${response.error}</code>`;
+
+        //certificateDetails
+        showCertificateDetails(certInfoObj, isValid, elementId = 'certificateDetails');
+
+        //certificate_details_title
+        hideElement('certificate_details_title', false);
+    }
+
+    // verificationResult callout 
+    createAlertElement('verificationResult', 'bd-callout', isValid, resultMessage);
+
+    //earningCriteria
+    hideElement('earningCriteria', isValid !== true);
+
+};
+
+
+function createAlertElement(elementId, classPrefix, isValid, resultMessage) {
+    document.getElementById(elementId).innerHTML = resultMessage;
+    if (isValid !== null) {
+        document.getElementById(elementId).classList.add(classPrefix, `${classPrefix}-${isValid ? 'success' : 'danger'}`);
+        document.getElementById(elementId).classList.remove(`${classPrefix}-${isValid ? 'danger' : 'success'}`);
+    } else {
+        document.getElementById(elementId).classList.add(classPrefix, `${classPrefix}-warning`);
+    }
+}
+
+function hideElement(elementId, wantToHide = true) {
+    let element = document.getElementById(elementId);
+    const display_style = window.getComputedStyle(element).display;
+    const has_hidden_attribute = element.hasAttribute('hidden');
+
+    if (!element) {
+        return;
+    }
+
+    if (wantToHide === true) {
+        if (!has_hidden_attribute) {
+            element.setAttribute('hidden', "");
+        }
+        if (display_style !== 'none') {
+            element.style.display = 'none';
+        }
+
+    } else {
+        if (has_hidden_attribute) {
+            element.removeAttribute('hidden');
+        }
+        if (display_style === 'none') {
+            element.style.display = 'block';
+        }
+
+    }
+
+}
+
+
+function showCertificateDetails(certInfoObj, isValid, elementId = 'certificateDetails') {
+
+    // for each key-value pair in certInfoObj
+    // create a li element with key as id and value as textContent
+    // append li element to ul element
+
+    // Get the ul element where we want to append the list items
+    const ul = document.getElementById(elementId);
+
+    hideElement(elementId, wantToHide = false);
+
+    //add the list item to the ul element --------------------
+
+    // Create a new li element
+    const li = document.createElement('li');
+
+    // Add the classes to the li element
+    li.classList.add('list-group-item', isValid ? 'list-group-item-success' : isValid === false ? 'list-group-item-danger' : 'list-group-item-warning',);
+
+    // Set the id of li to the key from certInfoObj
+    //li.id = key;
+
+    // append textContent="&check;" to li
+    li.textContent = `${isValid ? "Valid" : isValid === false ? "Invalid" : "Unverifiable"} certificate`;
+
+    // Append the newly created li to the ul element
+    ul.appendChild(li);
+
+    //---------------------------            
+
+    // Iterate over each entry in the object
+    for (const [key, value] of Object.entries(certInfoObj)) {
+        // Create a new li element
+        const li = document.createElement('li');
+
+        // Add the classes to the li element
+        li.classList.add('list-group-item', 'd-flex', 'align-items-center', /*'justify-content-between', */);
+
+        // Set the id of li to the key from certInfoObj
+        li.id = key;
+
+        /*
+        // Set the text content of li to the corresponding value from certInfoObj
+        li.textContent = `${key}: ${value}`;
+
+        //append a span element with class="badge bg-success rounded-pill" and textContent="&check;" to li
+        /*const span = document.createElement('span');
+        span.classList.add('badge', 'bg-success', 'rounded-pill');
+        span.innerHTML = '&check;';
+        li.appendChild(span);
+        */
+
+        // append textContent="&check;" to li
+        li.innerHTML = `<span class="badge ${isValid ? "bg-success" : isValid === false ? "bg-danger" : "bg-warning"} rounded-pill" style="margin-right:16px;">${isValid ? "&check;" : isValid === false ? "&cross;" : "&excl;"}</span>${key.replace("_", " ")}&nbsp;<b>${value}</b>`;
+
+
+        // Append the newly created li to the ul element
+        ul.appendChild(li);
+    }
+}
